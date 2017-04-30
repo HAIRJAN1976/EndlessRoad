c262f14b745542d9fec31ff692bb177872554906
HAIRJAN1976 <harijan1976@gmail.com>
2017-04-30 23:08:45 +0900

    CollisionのMesh修正
    ブロックのCollision Meshを基本Meshで設定。

--- Assets/Class/Manager/BlockManager.cs
+++ Assets/Class/Manager/BlockManager.cs
@@ -16,7 +16,9 @@ public class BlockManager : MonoBehaviour
         GameObject gameObject = (GameObject)Instantiate(model, new Vector3(x * 3, 0, y * 3), Quaternion.identity, transform);
         gameObject.name = type.ToString();
 
-        gameObject.AddComponent<MeshCollider>().sharedMesh = gameObject.transform.GetChild(0).GetComponent<MeshFilter>().sharedMesh;
+
+        //string path = string.Format("Block/naturePack_001.obj")
+        gameObject.AddComponent<MeshCollider>().sharedMesh = getFlatMesh();
 
         if (int.Parse(types[1]) > 5)
         {
@@ -27,5 +29,27 @@ public class BlockManager : MonoBehaviour
         gameObject.transform.tag = GameCode.GetTag(int.Parse(types[2]));
         
         return gameObject;
+    }
+
+    private static Mesh flatMesh;
+
+    private static Mesh getFlatMesh()
+    {
+        if(flatMesh == null)
+        {
+            string path = "Block/naturePack_001";
+            GameObject model = Resources.Load<GameObject>(path);
+            flatMesh = model.transform.GetChild(0).GetComponent<MeshFilter>().sharedMesh;
+
+            // 出力先ファイル名
+            //var filename = "Assets/Resources/Mesh/" + flatMesh.name + ".asset";
+
+            //// Assetへ保存
+            //UnityEditor.AssetDatabase.CreateAsset(flatMesh, filename);
+
+            //flatMesh = (Mesh)UnityEditor.AssetDatabase.LoadAssetAtPath(filename, typeof(Mesh));
+        }
+
+        return flatMesh;
     }
 }
